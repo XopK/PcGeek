b69a541532a8d4bdab2adddb2ba4e49e9e595683
XopK <dmahmutov12@gmail.com>
2023-11-22 11:56:09 +0500

    header

---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/style/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+    <title>PcGeek</title>
+</head>
+
+<body>
+    <div class="container">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
+            <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+                <h2>PcGeek</h2>
+            </a>
+
+            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link">Главная</a></li>
+                <li><a href="#" class="nav-link">Форум</a></li>
+                <li><a href="#" class="nav-link">Конструктор</a></li>
+            </ul>
+
+            <ul class="nav col-md-auto d-flex text-end">
+                <li><a href="#" class="nav-link">Регистрация</a></li>
+                <li><a href="#" class="nav-link">Вход</a></li>
+            </ul>
+        </header>
+
+        <main>
+            <h1>PcGeek</h1>
+            <h3>Собери свой компьютер сам</h3>
+        </main>
+    </div>
+</body>
+
+</html>
